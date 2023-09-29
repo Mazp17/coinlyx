34cc0b7722073118bf64691e8378a020e683fc5f
Miguel Zapata <zpmaepp@gmail.com>
2023-09-29 00:14:27 -0500

    lib(frontend): install bootstrap

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,15 @@
-import { useState } from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Button } from 'react-bootstrap';
+
 
 function App() {
 
   return (
     <>
       <h1>Coinlyx</h1>
+      <Button as="a" variant="primary">
+        Button as link
+      </Button>
     </>
   )
 }
